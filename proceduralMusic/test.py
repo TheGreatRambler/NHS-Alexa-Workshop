b59ce0cf397abebf6349886bf695f23706d9d5b7
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-01-03 17:28:11 -0500

    Update test.py

--- proceduralMusic/test.py
+++ proceduralMusic/test.py
@@ -5,9 +5,11 @@ from flask_ask import Ask, statement
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+mp3rootname = "something"
+
 @ask.intent('HelloIntent')
-def playnote(num):
-    return statement('')
+def playnote(notename):
+    return statement("<speak><audio src=" + mp3rootname + notename + ".mp3" /></speak>")
 
 if __name__ == '__main__':
     app.run(debug=True)
