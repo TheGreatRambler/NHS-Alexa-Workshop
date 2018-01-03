aeba0c98f886f5f3a88a72c61044785a8260d3b5
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-01-03 17:23:05 -0500

    Update test.py

--- proceduralMusic/test.py
+++ proceduralMusic/test.py
@@ -1,1 +1,14 @@
-print("Hello from Python!")
+import tensorflow as tf
+from flask import Flask, render_template
+from flask_ask import Ask, statement
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@ask.intent('HelloIntent')
+def playnote(num):
+    return statement('')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
