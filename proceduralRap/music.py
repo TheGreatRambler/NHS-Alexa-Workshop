2b11e16ce1becef86fb3b563fc4bc3fe3cdb449f
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-01-08 15:23:22 -0500

    Update music.py

--- proceduralRap/music.py
+++ proceduralRap/music.py
@@ -1,15 +1,34 @@
-import random
 from flask import Flask, render_template
 from flask_ask import Ask, statement
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+noteletter = "a"
+
+# songspeeds greater than 100 are faster and less than 100 are slower: 20 is the minimum speed
+songspeed = 150
+
+def returnnote(pitch, duration):
+    transformedpitch = ""
+    if pitch < 30:
+        num = str(pitch + 30)
+        transformedpitch = "-" + num + "%"
+    elif pitch > 30:
+        num = str(pitch - 30)
+        transformedpitch = "+" + num + "%"
+    elif pitch == 30:
+        transformedpitch = "+0%"
+    return "<prosody pitch='" + transformedpitch + "' rate='" + str(duration) + "%'>" + noteletter + "</prosody>"
+
 @ask.launch
             
 def launched():
-    welcome_message = "Give me something ta rap, dawg."
-    return statement(welcome_message)
+    song = ""
+    for x in range(0, 8):
+         song += returnnote(x * 10, songspeed)
+    music = "<speak>" + song + "</speak>"
+    return statement(music)
 
 if __name__ == '__main__':
     app.run(debug=True)
