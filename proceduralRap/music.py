af9c3e23a53365c2dd115ec1b644619bb059d199
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-01-08 13:21:44 -0500

    Update music.py

--- proceduralRap/music.py
+++ proceduralRap/music.py
@@ -1,1 +1,15 @@
+import random
+from flask import Flask, render_template
+from flask_ask import Ask, statement
 
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@ask.launch
+            
+def launched():
+    welcome_message = "Give me something ta rap, dawg."
+    return statement(welcome_message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
